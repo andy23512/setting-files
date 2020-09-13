58aaeb91f7d6045b5e7838d304f7ee415758c01d
andy23512 <andy23512@gmail.com>
2020-09-14 01:48:39 +0800

    Add diff-so-fancy and git-jump

--- install-deps.sh
+++ install-deps.sh
@@ -8,6 +8,7 @@ sudo apt-get install vim python3 python3-venv python3-pip python-virtualenv zsh
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
+npm install -g git-jump
 
 # install yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
@@ -17,4 +18,5 @@ sudo apt-get update && sudo apt-get install yarn
 # install yarn completion
 yarn global add yarn-completions
 
-
+# install diff-so-dancy
+sudo apt-get install -y diff-so-fancy
