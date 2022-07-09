01b69b1ce8d91857d9067dd95ebff8fff1a70a03
andy23512 <andy23512@gmail.com>
2022-07-10 00:09:31 +0800

    feat: add enlarge keys and reload config

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
+
 keys = {
 	['1'] = {index = 0, split = 4},
 	['2'] = {index = 1, split = 4},
@@ -13,6 +27,12 @@ keys = {
 	['='] = {index = 4, split = 5},
 	['['] = {index = 0, split = 2},
 	[']'] = {index = 1, split = 2},
+}
+
+enlarge_keys = {
+	['q'] = 2,
+	['w'] = 3,
+	['e'] = 4,
 }
 
 hs.window.animationDuration = 0
@@ -33,5 +53,28 @@ end
 for key, val in pairs(keys) do
 	hs.hotkey.bind({'cmd', 'alt'}, key, function()
 		moveWindow(val.index, val.split)
+	end)
+end
+
+function enlargeWindowWidth(n)
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local screen = win:screen()
+	local max = screen:frame()
+
+	local width = f.w * n
+
+	if f.x + width > max.w then
+		f.x = max.w - width
+	end
+
+	f.w = f.w * n
+
+	win:setFrame(f)
+end
+
+for key, val in pairs(enlarge_keys) do
+	hs.hotkey.bind({'cmd', 'alt'}, key, function()
+		enlargeWindowWidth(val)
 	end)
 end
